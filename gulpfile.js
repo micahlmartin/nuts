c81f0d543fab19d046b79e490331ed45b23f1eba
Micah Martin <micahlmartin@gmail.com>
2014-11-07 00:14:35 -0500

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,15 @@ var config = {
 
 function handleErrors() {
   var args = Array.prototype.slice.call(arguments);
-  $.notify.onError({
-    title: "Compile Error",
-    message: "<%= error.message %>"
-  }).apply(this, args);
+  if(config.isProduction) {
+    console.log(args);
+  } else {
+    $.notify.onError({
+      title: "Compile Error",
+      message: "<%= error.message %>"
+    }).apply(this, args);
+  }
+
   this.emit('end'); // Keep gulp from hanging on this task
 }
 
