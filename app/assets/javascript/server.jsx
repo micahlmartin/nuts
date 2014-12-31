62a88b216117ccb8cfb28fbc87c6efc8ef37aad7
Micah Martin <micahlmartin@gmail.com>
2014-12-30 21:48:02 -0500

    reverted

--- app/assets/javascript/server.jsx
+++ app/assets/javascript/server.jsx
@@ -8,8 +8,8 @@ var _               = require('lodash');
 var fs              = require('fs');
 var path            = require('path');
 
-var layoutCache = [];
-var layoutsDir = path.join(Nuts.root, 'app', 'assets', 'javascript', 'views', 'layouts');
+var template = null;
+var templateFilePath = path.join(Nuts.root, 'app', 'assets', 'javascript', 'views', 'layouts', Nuts.settings.layout + '.template');
 
 var renderTemplate = function(template, context) {
   return _.template(template, context)
@@ -18,36 +18,31 @@ var renderTemplate = function(template, context) {
 module.exports = function(assetFilename, context) {
   var deferred = defer();
 
-  // Use the layout passed into the view context if it exists or the default one
-  var layoutName = context.layout || Nuts.settings.layout;
-
   Main.renderServer(context.path, function(Handler) {
     var html;
     var css = styleCollector.collect(function() {
       html = React.renderToString(<Handler {...context} />);
     });
 
-    viewContext = _.assign(context, {
+    context = _.assign(context, {
       css: css,
       html: html,
       assetFilename: assetFilename,
       data: utilities.safeStringify(context)
     });
 
-    var layout = layoutCache[layoutName];
-    if(!layout) {
-      fs.readFile(path.join(layoutsDir, layoutName + '.template'), function(err, data) {
+    if(!template) {
+      fs.readFile(templateFilePath, function(err, data) {
         if(err) {
           Nuts.reportError(err, true);
         }
 
-        layout = data.toString();
-        layoutCache[layoutName] = layout;
+        template = data.toString();
 
-        deferred.resolve(_.template(layout, viewContext));
+        deferred.resolve(_.template(template, context));
       });
     } else {
-      deferred.resolve(_.template(layout, viewContext));
+      deferred.resolve(_.template(template, context));
     }
 
   });
